610a0200b25e1cc333c47ea53930609efb18b5f0
Patrick Pontes <patrickhisoka@gmail.com>
2020-07-02 13:27:52 -0300

    segundo commit

---
+++ build/precache-manifest.9b837c511e9a82b1ca644cdd906efe1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28d1488567b107b26fa256b39e0944f",
+    "url": "/PatrickPontes44/Inspire.se/index.html"
+  },
+  {
+    "revision": "19c2abaa7e0c9ec316f1",
+    "url": "/PatrickPontes44/Inspire.se/static/css/main.876a994a.chunk.css"
+  },
+  {
+    "revision": "6f26288fbd969605c1a3",
+    "url": "/PatrickPontes44/Inspire.se/static/js/2.da6a0e7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PatrickPontes44/Inspire.se/static/js/2.da6a0e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c2abaa7e0c9ec316f1",
+    "url": "/PatrickPontes44/Inspire.se/static/js/main.79847e6b.chunk.js"
+  },
+  {
+    "revision": "0396d0a7da75aaba871b",
+    "url": "/PatrickPontes44/Inspire.se/static/js/runtime-main.f117c5b9.js"
+  },
+  {
+    "revision": "376c5d4da976a316f2f4b59679432d96",
+    "url": "/PatrickPontes44/Inspire.se/static/media/bg.376c5d4d.png"
+  },
+  {
+    "revision": "30679830b1e7ce273cf3395ec5b46a52",
+    "url": "/PatrickPontes44/Inspire.se/static/media/bg2.30679830.png"
+  }
+]);
